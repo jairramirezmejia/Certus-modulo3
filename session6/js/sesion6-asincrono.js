007f1495e6a13f35e4ba060f22833fd83b4dfcd7
Jair Ramirez <cyberal159@gmail.com>
2022-12-02 12:19:31 -0500

    Sesion 7 - formularios y validaciones

---
+++ session6/js/sesion6-asincrono.js
@@ -0,0 +1,14 @@
+const usuario = document.getElementById("txtUsuario");
+const valUsuario = document.getElementById("valUsuario");
+
+const nombre = document.getElementById("txtNombre");
+const valNombre= document.getElementById("valNombre");
+
+const contraseña = document.getElementById("txtContraseña");
+const valContraseña= document.getElementById("valContraseña");
+
+const pais = document.getElementById("selPais");
+const valpais = document.getElementById("valPais");
+
+const chkaceptar = document.getElementById("chkAceptar");
+const valaceptar = document.getElementById("valAceptar");
