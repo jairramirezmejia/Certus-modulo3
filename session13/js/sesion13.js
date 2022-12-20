29cd02fe830242870da94c57fbad00de25c6db30
Jair Ramirez <cyberal159@gmail.com>
2022-12-20 14:34:06 -0500

    AngularJS

---
+++ session13/js/sesion13.js
@@ -0,0 +1,16 @@
+let app = angular.module("MyApp",[]);
+
+app.controller("formCtrl", function($scope){
+  $scope.master = { firstName: "John", lastName: "Doe" }
+  $scope.reset = function(){
+    $scope.user = angular.copy($scope.master);
+  }
+  $scope.reset();
+})
+
+// La empresa “MULTIPLAN” dedicada al alquiler de vehículos, emite una factura de acuerdo al servicio prestado a sus clientes por la distancia recorrida en el auto alquilado.
+//Si la distancia recorrida no es mayor a los 300 km., se cobra una tarifa fija de S/.250,
+// si la distancia recorrida es mayor a 300 km. y hasta 1000 km. se cobra la tarifa fija más el exceso de kilómetros a razón de S/. 30 por km. y
+// si la distancia recorrida es mayor a 1000 km., la compañía cobra la tarifa fija más los kilómetros recorridos entre 300 hasta 1000 a razón de S/. 30, más S/.20 por Kilómetro de exceso en distancias mayores de 1000 km.
+// Además, por promoción otorga un descuento del 10% del importe a pagar, por importes mayores a 500 soles.
+
