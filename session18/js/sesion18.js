987730334de02bf1ccbd997120e5640d909812dc
Jair Ramirez <cyberal159@gmail.com>
2022-12-27 16:25:13 -0500

    seleccion multiple

--- session18/js/sesion18.js
+++ session18/js/sesion18.js
@@ -1,47 +1,38 @@
-// 1. Solicitar a la Poke API la informacion de los pokemons de la generacion 1.
-// 2. Con un Select, seleccionar el pokemon que se desee ver en pantalla.
-// 3. Solicitar a la Poke API el sprite y data del pokemon.
-// 4. Renderizar en pantalla la foto del pokemon y su nombre, centrados al medio de la pagina.
+// --- POKEMON LIST --- //
+// Esta página es similar a la que hicimos en clase: Debe mostrar un selector con los nombres de los pokemones de la generación 1 y una imagen del pokemon con su nombre al medio de la pantalla.
+// Debe existir un botón: “agregar a favoritos” para agregarlo a la base de datos de pokemons favoritos y se debe solicitar confirmación del usuario.
+// Al tenerlo de favorito el background del nombre del pokemon debe cambiar de color para que el usuario pueda distinguir que dicho pokemon es parte de sus favoritos.
 
-const resultPokemons = document.getElementById('root');
+const resultPokemons = document.getElementById('container');
+const url = "https://pokeapi.co/api/v2/generation/1";
 
-function fetchPokemons(params) {
-  for (let i = 1; i <= params; i++) {
-    fetchPokemon(i);
+fetch(url)
+.then(response => response.json())
+.then(data => viewPokemons(data))
+.catch(error => console.error(error))
+
+function viewPokemons (params) {
+  let pokemon = params.pokemon_species.map( element => {
+    return element.name
+  })
+  let select = document.createElement("select");
+  select.classList.add('select-pokemon')
+
+  for (const val of pokemon) {
+    let option = document.createElement("option");
+    option.value = val;
+    option.text = val.charAt(0).toUpperCase() + val.slice(1);
+    select.appendChild(option);
   }
+
+  let label = document.createElement("label");
+  label.innerHTML = "Selecciona un pokemon: "
+  resultPokemons.appendChild(label).appendChild(select);
 }
 
-function fetchPokemon(id) {
-  fetch(`https://pokeapi.co/api/v2/generation/1`)
-  .then(response => response.json())
-  .then(data =>
-    createPokemon(data)
-  )
-};
+// ----------------------------------------
 
-function createPokemon(pokemon) {
-  const card = document.createElement('section');
-  card.classList.add('card-pokemon');
-
-  const spriteContaniner = document.createElement('section');
-  spriteContaniner.classList.add('img-container');
-
-  // const sprite = document.createElement('img');
-  // sprite.src = pokemon.sprites.front_default;
-
-  const name = document.createElement('p');
-  name.classList.add('name');
-  name.textContent = pokemon.pokemon_species[0].name;
-
-  // card.appendChild(sprite);
-  card.appendChild(name);
-  resultPokemons.appendChild(card);
-}
-
-fetchPokemons(5);
-
-
-// const resultPokemons = document.getElementById('root');
+// fetchPokemons(9);
 
 // function fetchPokemons(params) {
 //   for (let i = 1; i <= params; i++) {
@@ -53,29 +44,23 @@ fetchPokemons(5);
 //   fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
 //   .then(response => response.json())
 //   .then(data =>
-//     console.log(data)
+//     createPokemon(data)
 //   )
 // };
 
 // function createPokemon(pokemon) {
+
 //   const card = document.createElement('section');
 //   card.classList.add('card-pokemon');
 
-//   const spriteContaniner = document.createElement('section');
-//   spriteContaniner.classList.add('img-container');
-
-//   const sprite = document.createElement('img');
-//   sprite.src = pokemon.sprites.front_default;
-
-//   console.log(pokemon.sprites.front_default);
+//   const img = document.createElement('img');
+//   img.src = pokemon.sprites.front_default;
 
 //   const name = document.createElement('p');
-//   name.classList.add('name');
+//   name.classList.add('name-pokemon');
 //   name.textContent = pokemon.name;
 
-//   card.appendChild(sprite);
+//   card.appendChild(img);
 //   card.appendChild(name);
 //   resultPokemons.appendChild(card);
 // }
-
-// fetchPokemons(9);
