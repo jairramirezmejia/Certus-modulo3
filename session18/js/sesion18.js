1460917e23243b1d2d8c1cf1b3b3953cfe754bc0
Jair Ramirez <cyberal159@gmail.com>
2022-12-28 19:23:06 -0500

    api pokemon

--- session18/js/sesion18.js
+++ session18/js/sesion18.js
@@ -4,63 +4,53 @@
 // Al tenerlo de favorito el background del nombre del pokemon debe cambiar de color para que el usuario pueda distinguir que dicho pokemon es parte de sus favoritos.
 
 const resultPokemons = document.getElementById('container');
-const url = "https://pokeapi.co/api/v2/generation/1";
+const url = "https://pokeapi.co/api/v2/";
+const resultCard = document.getElementById('resultCard');
+const viewPokemonSelection = document.getElementById('viewPokemonSelection');
 
-fetch(url)
-.then(response => response.json())
-.then(data => viewPokemons(data))
-.catch(error => console.error(error))
+fetch(url + "generation/1")
+  .then(response => response.json())
+  .then(data => createListPokemon(data));
 
-function viewPokemons (params) {
-  let pokemon = params.pokemon_species.map( element => {
+function createListPokemon(pokemon) {
+  const arrayPokemon = pokemon.pokemon_species.map (element => {
     return element.name
-  })
+  });
+  let form = document.createElement("form");
+  let label = document.createElement("label");
+  label.innerText = "Selecciona un pokemon: "
   let select = document.createElement("select");
-  select.classList.add('select-pokemon')
-
-  for (const val of pokemon) {
+  select.setAttribute("id",'selectPokemon')
+  for (const val of arrayPokemon) {
     let option = document.createElement("option");
     option.value = val;
     option.text = val.charAt(0).toUpperCase() + val.slice(1);
     select.appendChild(option);
   }
-
-  let label = document.createElement("label");
-  label.innerHTML = "Selecciona un pokemon: "
-  resultPokemons.appendChild(label).appendChild(select);
+  resultPokemons.appendChild(form).appendChild(label).appendChild(select);
 }
 
-// ----------------------------------------
+viewPokemonSelection.onclick = (event) => {
+  event.preventDefault();
+  let namePokemon = document.getElementById('selectPokemon');
+  resultCard.innerHTML =`<p> ${namePokemon.value.toUpperCase()} </p>`;
+  fetch(url + "pokemon/" + namePokemon.value)
+    .then(response => response.json())
+    .then (data => imgPokemon(data))
+    .catch(error => console.error(error));
+}
 
-// fetchPokemons(9);
+function imgPokemon(pokemon) {
+  const imgPokemon = document.createElement('img');
+  imgPokemon.setAttribute("id", "img-pokemon");
+  imgPokemon.src = pokemon.sprites.other.home.front_default;
+  resultCard.appendChild(imgPokemon);
+}
 
-// function fetchPokemons(params) {
-//   for (let i = 1; i <= params; i++) {
-//     fetchPokemon(i);
-//   }
-// }
-
-// function fetchPokemon(id) {
-//   fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-//   .then(response => response.json())
-//   .then(data =>
-//     createPokemon(data)
-//   )
-// };
-
-// function createPokemon(pokemon) {
-
-//   const card = document.createElement('section');
-//   card.classList.add('card-pokemon');
-
-//   const img = document.createElement('img');
-//   img.src = pokemon.sprites.front_default;
-
-//   const name = document.createElement('p');
-//   name.classList.add('name-pokemon');
-//   name.textContent = pokemon.name;
-
-//   card.appendChild(img);
-//   card.appendChild(name);
-//   resultPokemons.appendChild(card);
-// }
+addFavoritePokemon.onclick = (event) => {
+  if (resultCard.classList.contains('change-background')) {
+    resultCard.classList.remove('change-background')
+  } else {
+    resultCard.classList.add('change-background');
+  }
+}
